920ebbe486306eb20833163054859442a548272e
Gustavo Niemeyer <gustavo@niemeyer.net>
2017-02-10 17:09:08 -0200

    Do not show seed value when discarding.

--- spread/runner.go
+++ spread/runner.go
@@ -197,7 +197,7 @@ func (r *Runner) loop() (err error) {
 	logNames(debugf, msg, r.pending, taskName)
 
 	seed := r.options.Seed
-	if seed == 0 && len(r.pending) > r.alive {
+	if !r.options.Discard && seed == 0 && len(r.pending) > r.alive {
 		seed = time.Now().Unix()
 		printf("Sequence of jobs produced with -seed=%d", seed)
 	}

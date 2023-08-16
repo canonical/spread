0ae4a6075dccecb5778f61a913368cc6cd8f7937
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2023-08-16 18:36:17 -0300

    Fixes after testing

--- spread/project.go
+++ spread/project.go
@@ -120,7 +120,8 @@ type System struct {
 	Workers  int
 
 	// Only for Testflinger so far.
-	Queue	string
+	Queue       string
+	WaitTimeout Timeout `yaml:"wait-timeout"`
 
 	// Only for Linode and Google so far.
 	Storage Size
